6c305304b576d42ddfd26c5103162a79451df553
Christian Ainsley Del Rosario <ainsley.drosario@gmail.com>
2023-09-28 14:26:30 +0800

    replaced previous react app with vite+react

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello World</h1>
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
