2566af0f3e45af054eedfed3132aeda0cad35c97
Christian Ainsley Del Rosario <ainsley.drosario@gmail.com>
2023-10-13 17:27:49 +0800

    finished Dashboard page

--- client/src/components/JobUpdates.jsx
+++ client/src/components/JobUpdates.jsx
@@ -10,26 +10,7 @@ export default function JobUpdates() {
         <strong className="font-bold text-xl">View All</strong>
       </div>
       <div className="flex gap-2 py-4">
-        <BoxWrapper>
-          <div className="flex flex-row justify-between items-center py-4">
-            <Avatar src="src/assets/dell-logo.svg" size="xl" />
-            <div className="bg-light-yellow rounded-lg p-3">
-              <span className="font-bold">Full Time</span>
-            </div>
-          </div>
-          <div className="flex flex-col py-4">
-            <span className="font-bold">Social Media Assistant</span>
-            <span className="font-bold">Dell - Philippines</span>
-          </div>
-          <div className="flex flex-row gap-1.5">
-            <div className="border border-dark-yellow rounded-lg p-3">
-              <span className="font-bold text-dark-yellow">Marketing</span>
-            </div>
-            <div className="border border-green rounded-lg p-3">
-              <span className="font-bold text-green">Design</span>
-            </div>
-          </div>
-        </BoxWrapper>
+        <BoxWrapper></BoxWrapper>
         <BoxWrapper></BoxWrapper>
         <BoxWrapper></BoxWrapper>
         <BoxWrapper></BoxWrapper>
@@ -41,7 +22,32 @@ export default function JobUpdates() {
 function BoxWrapper({ children }) {
   return (
     <div className="bg-white rounded-md border border-gray p-4 h-[20rem] flex-1">
-      {children}
+      <div className="flex flex-row justify-between items-center py-4">
+        <Avatar src="src/assets/dell-logo.svg" size="xl" />
+        <div className="bg-light-yellow rounded-lg p-3">
+          <span className="font-bold">Full Time</span>
+        </div>
+      </div>
+      <div className="flex flex-col py-4">
+        <span className="font-bold">Social Media Assistant</span>
+        <span className="font-bold text-sm">Dell - Philippines</span>
+      </div>
+      <div className="flex flex-row gap-1.5">
+        <div className="border border-dark-yellow rounded-lg p-3">
+          <span className="font-bold text-dark-yellow">Marketing</span>
+        </div>
+        <div className="border border-green rounded-lg p-3">
+          <span className="font-bold text-green">Design</span>
+        </div>
+      </div>
+      <div className="py-3">
+        <div className="h-2 w-full bg-white border">
+          <div className="h-2 bg-dark-yellow" style={{ width: "50%" }}></div>
+          <div className="py-2">
+            <span className="text-sm">5 Applied out of 10 Applicants</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
